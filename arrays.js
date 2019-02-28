889f1433383125f4c213d1f0c02ebe93fdc63449
han xu <hanxu27@gmail.com>
2019-02-28 23:42:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(arr, b){
 }
 
 function addElementToEndOfArray(arr, b){
-  var Newarr = [...arr, b]
+  var newArr = [...arr, b]
   return newArr
 }
 
